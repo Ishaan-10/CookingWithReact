c18b7916bbd9705107bf7ac5a9614a0c2b1b498f
Ishaan-10 <b.ishaan10@gmail.com>
2021-07-03 21:08:36 +0530

    edit page styling

--- src/Components/ReciepeEdit.js
+++ src/Components/ReciepeEdit.js
@@ -4,27 +4,31 @@ import ReciepeIngredients from './ReciepeIngredients'
 export default function ReciepeEdit() {
     return (
         <div className="reciepe_edit">
-            <button>&times;</button>
-            <h1>Edit Section</h1>
-            <div>
-                <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name" />
-                <label htmlFor="Cook-Time">Cook-Time</label>
-                <input type="text" name="Cook-Time" id="Cook-Time" />
-                <label htmlFor="name">servings</label>
-                <input type="number" name="servings" id="servings" min="1" />
-                <label htmlFor="instructions">instructions</label>
-                <textarea name="instructions" id="instructions" cols="30" rows="10"></textarea>
+            <div className="remove-button-container">
+            <button className="btn" style={{backgroundColor:"red"}}>&times;</button>
+            </div>
+
+            <h2>Edit Section</h2>
+            <div className="reciepe_edit-grid">
+                <label htmlFor="name" className="reciepe_edit-label">Name</label>
+                <input type="text" name="name" id="name" className="reciepe_edit-edit"/>
+                <label htmlFor="Cook-Time" className="reciepe_edit-label">Cook-Time</label>
+                <input type="text" name="Cook-Time" id="Cook-Time" className="reciepe_edit-edit" />
+                <label htmlFor="name" className="reciepe_edit-label">Servings</label>
+                <input type="number" name="servings" id="servings" min="1" className="reciepe_edit-edit" />
+                <label htmlFor="instructions" className="reciepe_edit-label">Instructions</label>
+                <textarea name="instructions" id="instructions" cols="30" rows="10" className="reciepe_edit-edit"></textarea>
             </div>
             <br />
-            <label>Ingredients</label>
-            <div>
+            <label className="reciepe_edit-label">Ingredients</label>
+            <div className="reciepe-edit-ingredients">
                 <div>Name</div>
                 <div>Amount</div>
                 <div></div>
             </div>
-            <button>Add Ingredient</button>
             <ReciepeIngredients />
+            <button className="btn">Add Ingredient</button>
+
         </div>
     )
 }
--- src/Components/ReciepeIngredients.js
+++ src/Components/ReciepeIngredients.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function ReciepeIngredients() {
     return (
         <div>
-            <input type="text" />
-            <input type="text" />
-            <button>&times;</button>
+            <input type="text" className="reciepe_edit-edit"/>
+            <input type="text" className="reciepe_edit-edit"/>
+            <button className="btn">&times;</button>
         </div>
     )
 }
